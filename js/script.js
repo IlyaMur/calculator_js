1b0b64f81d8575e62e01c67178b5b6b5087a9035
Ilya Mur <ilyamur@hotmail.com>
2021-07-30 20:15:16 +0300

    fix vars

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let money = 50000;
-let income = "15000";
-let addExpenses = "1000, 2000, 5000";
+let income = "freelance";
+let addExpenses = "такси, собака, ЗОЖ";
 let deposit = true;
 let mission = 1000000;
 let period = 6;
 let budgetDay = money / 30;
 
-console.log(money);
-console.log(income);
-console.log(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
