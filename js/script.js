39300bfc54de9a4da5d5ed45064cb0b344f8c3db
Ilya Mur <ilyamur@hotmail.com>
2021-07-30 12:51:08 +0300

    add lesson 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let income = "15000";
+let addExpenses = "1000, 2000, 5000";
+let deposit = true;
+let mission = 1000000;
+let period = 6;
+let budgetDay = money / 30;
 
-console.log("JS is awesome!");
-alert("JS is awesome!");
+console.log(money);
+console.log(income);
+console.log(deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLowerCase().split(", "));
+console.log(budgetDay);
