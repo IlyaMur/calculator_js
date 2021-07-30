4efaf3313238fdaef4923129f2847f0cb060afe4
Ilya Mur <ilyamur@hotmail.com>
2021-07-30 23:51:39 +0300

    refactor formatting

--- js/script.js
+++ js/script.js
@@ -6,11 +6,23 @@ let mission = 1000000;
 let period = 6;
 let budgetDay = money / 30;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(
+  `money: ${typeof money}`,
+  `\nincome: ${typeof income}`,
+  `\ndeposit: ${typeof deposit}`
+);
+
 console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
-console.log(addExpenses.toLowerCase().split(", "));
+
+console.log(
+  `Период равен ${period} месяцев`,
+  `\nЦель заработать ${mission} рублей/долларов/гривен/юани`
+);
+
+console.log(
+  addExpenses.
+    toLowerCase().
+    split(", ")
+);
+
 console.log(budgetDay);
