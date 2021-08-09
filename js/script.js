6f4c06161f83f636b738c684f86780a30234e04c
Ilya Mur <ilyamur@hotmail.com>
2021-08-09 12:02:10 +0300

    add lesson09

--- js/script.js
+++ js/script.js
@@ -154,3 +154,54 @@ console.log('Наша программа включает в себя данны
 for (let i in appData) {
   console.log(i, appData[i]);
 }
+
+
+
+//-----------------------  LESSON 09
+
+
+// Кнопку "Рассчитать" через id
+let calculateBtn = document.getElementById('start');
+
+// Кнопки “+” (плюс) через Tag, каждую в своей переменной. 
+// Раз просили через 'Tag', сделал вместо querySelector через getElementsByTagName
+let buttons = document.getElementsByTagName('button');
+let incomeAddButton;
+let expensesAddButton;
+for (let element of buttons) {
+  if (element.className === 'btn_plus expenses_add') {
+    expensesAddButton = element;
+  } else if (element.className === 'btn_plus income_add') {
+    incomeAddButton = element;
+  }
+}
+
+// Чекбокс по id через querySelector
+let depositCheckbox = document.querySelector('#deposit-check');
+
+// Поля для ввода возможных доходов (additional_income-item) при помощи querySelectorAll
+let additionalIncomeItems = document.querySelectorAll('.additional_income-item');
+let firtsIncomeForm = additionalIncomeItems[0];
+let secondIncomeForm = additionalIncomeItems[1];
+
+// Каждый элемент в правой части программы через класс(не через querySelector), 
+// которые имеют в имени класса "-value", начиная с class="budget_day-value" 
+// и заканчивая class="target_month-value">
+let budgetDay = document.getElementsByClassName('budget_day-value')[0];
+let expensesMonth = document.getElementsByClassName('expenses_month-value')[0];
+let additionalIncome = document.getElementsByClassName('additional_income-value')[0];
+let additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0];
+let incomePeriod = document.getElementsByClassName('income_period-value')[0];
+let targetMonth = document.getElementsByClassName('target_month-value')[0];
+
+// Оставшиеся поля через querySelector каждый в отдельную переменную:
+// поля ввода(input) с левой стороны и не забудьте про range.
+let budgetMonth = document.querySelector('.budget_month-value');
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-items>.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let expensesTitle = document.querySelector('.expenses-items>.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let targetAmount = document.querySelector('.target-amount');
+let periodSelect = document.querySelector('.period-select');
