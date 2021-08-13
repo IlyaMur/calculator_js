6cf5b382b4a5b8b3168d4ba694c8751baac04d8e
Ilya Mur <ilyamur@hotmail.com>
2021-08-13 16:08:28 +0300

    add 14-bonus

--- js/script.js
+++ js/script.js
@@ -1,51 +1,104 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
-
-DomElement.prototype.createElement = function () {
-  switch (this.selector[0]) {
-    case '.':
-      const div = document.createElement('div');
-      div.classList.add('div-element');
-      document.body.append(div);
-      div.textContent = 'BY CLASS';
-
-      this.addStyles(div);
-
-      break;
-    case '#':
-      const p = document.createElement('p');
-      p.id = 'p-element';
-      document.body.append(p);
-      p.textContent = 'BY ID';
-
-      this.addStyles(p);
-
-      break;
+document.addEventListener('DOMContentLoaded', function () {
+  function DomElement(selector, height, width, bg, fontSize, position) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.position = position;
   }
-};
 
-DomElement.prototype.addStyles = function (element) {
-  element.style.cssText = `
+  DomElement.prototype.createElement = function () {
+    switch (this.selector[0]) {
+      case '.':
+        const div = document.createElement('div');
+        div.classList.add(this.selector.slice(1));
+        document.body.append(div);
+        div.textContent = 'BY CLASS';
+
+        this.addStyles(div);
+
+        break;
+      case '#':
+        const p = document.createElement('p');
+        p.id = this.selector.slice(1);
+        document.body.append(p);
+        p.textContent = 'BY ID';
+
+        this.addStyles(p);
+
+        break;
+    }
+  };
+
+  DomElement.prototype.addStyles = function (element) {
+    element.style.cssText = `
     height: ${this.height};
     background-color: ${this.bg};
     width: ${this.width};
     font-size: ${this.fontSize};
+    position: ${this.position};
+    margin: 0
     `;
-};
+  };
 
-const domElement = new DomElement(
-  '#my-id',
-  '30px',
-  '200px',
-  'red',
-  '25px'
-);
+  const domElement = new DomElement(
+    '.my-id',
+    '30px',
+    '200px',
+    'red',
+    '25px'
+  );
 
-domElement.createElement();
+  domElement.createElement();
+
+  // -------
+
+  const square = new DomElement(
+    '#square',
+    '100px',
+    '100px',
+    'yellow',
+    '25px',
+    'absolute'
+  );
+
+  square.createElement();
+
+  // ---------
+
+  const squareElement = document.getElementById('square');
+
+  let counterLeft = 0;
+  let counterUp = 0;
+
+  document.addEventListener('keydown', function (e) {
+    switch (e.key) {
+      case "ArrowLeft":
+        counterLeft--;
+        squareElement.style.marginLeft = `${counterLeft}px`;
+
+        break;
+      case "ArrowRight":
+        counterLeft++;
+        squareElement.style.marginLeft = `${counterLeft}px`;
+
+        break;
+      case "ArrowUp":
+        counterUp--;
+        squareElement.style.marginTop = `${counterUp}px`;
+
+        break;
+      case "ArrowDown":
+        counterUp++;
+        squareElement.style.marginTop = `${counterUp}px`;
+
+        break;
+    }
+  });
+});
+
+
+
